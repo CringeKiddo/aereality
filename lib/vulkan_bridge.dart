d129298e760aefffea6d6537ccc27d3e66028c80
DantaeTeeHee <99839831+CringeKiddo@users.noreply.github.com>
2026-08-05 11:13:13 -0500

    Update vulkan_bridge.dart

--- lib/vulkan_bridge.dart
+++ lib/vulkan_bridge.dart
@@ -9,22 +9,22 @@ final DynamicLibrary nativeLib = Platform.isAndroid
     ? DynamicLibrary.open('libvulkan_processor.so')
     : DynamicLibrary.process();
 
-// --- FFI function signatures using correct types ---
-typedef InitNative = Void Function(Pointer<Uint32> spirv, IntPtr size);
-typedef ProcessNative = Void Function(Pointer<Uint8> input, Int32 w, Int32 h, Pointer<Uint8> output);
+// --- FFI function signatures (Dart types) ---
+typedef InitNative = Void Function(Pointer<Uint32> spirv, int size);
+typedef ProcessNative = Void Function(Pointer<Uint8> input, int w, int h, Pointer<Uint8> output);
 typedef CleanupNative = Void Function();
 
-// --- Dart bindings with explicit casting ---
+// --- Dart bindings ---
 final initProcessor = nativeLib
-    .lookup<NativeFunction<InitNative>>('init_processor')
+    .lookup<NativeFunction<Void Function(Pointer<Uint32>, IntPtr)>>('init_processor')
     .asFunction<InitNative>();
 
 final processFrame = nativeLib
-    .lookup<NativeFunction<ProcessNative>>('process_frame')
+    .lookup<NativeFunction<Void Function(Pointer<Uint8>, Int32, Int32, Pointer<Uint8>)>>('process_frame')
     .asFunction<ProcessNative>();
 
 final cleanupProcessor = nativeLib
-    .lookup<NativeFunction<CleanupNative>>('cleanup_processor')
+    .lookup<NativeFunction<Void Function()>>('cleanup_processor')
     .asFunction<CleanupNative>();
 
 // --- Global state ---
@@ -33,11 +33,10 @@ bool _initialized = false;
 /// Initialize the Vulkan processor with the SPIR-V shader binary.
 void initVulkan(Uint8List spirv) {
   if (_initialized) return;
-  // Allocate memory for the SPIR-V binary (size in bytes)
   final ptr = calloc<Uint32>(spirv.length ~/ 4);
   ptr.asTypedList(spirv.length ~/ 4).setAll(0, spirv.buffer.asUint32List());
-  // Pass size as IntPtr (size_t)
-  initProcessor(ptr, IntPtr(spirv.length));
+  // Pass size as int – Dart FFI converts to size_t automatically
+  initProcessor(ptr, spirv.length);
   calloc.free(ptr);
   _initialized = true;
 }
@@ -46,21 +45,17 @@ void initVulkan(Uint8List spirv) {
 Uint8List processImage(Uint8List input, int width, int height) {
   if (!_initialized) throw Exception('Vulkan not initialized. Call initVulkan() first.');
   
-  // Allocate input buffer
   final inputPtr = calloc<Uint8>(input.length);
   inputPtr.asTypedList(input.length).setAll(0, input);
   
-  // Allocate output buffer
   final output = Uint8List(width * height * 4);
   final outputPtr = calloc<Uint8>(output.length);
   
-  // Process – cast width/height to Int32
-  processFrame(inputPtr, Int32(width), Int32(height), outputPtr);
+  // Pass width/height as int – Dart FFI converts to int32_t automatically
+  processFrame(inputPtr, width, height, outputPtr);
   
-  // Copy output back
   output.setAll(0, outputPtr.asTypedList(output.length));
   
-  // Free memory
   calloc.free(inputPtr);
   calloc.free(outputPtr);
   
